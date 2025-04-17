b20c86a44464c941911f24011ad78b7ca23cb0e9
masa18-fr <Fuwaryura18.ktz.fr+github@gmail.com>
2025-04-17 18:25:38 +0900

    vite change

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/high-low-game/', // 👈 GitHub Pagesのサブパス
+  base: '/high-low-game/', //  GitHub Pagesのサブパス
   plugins: [react()],
 })
